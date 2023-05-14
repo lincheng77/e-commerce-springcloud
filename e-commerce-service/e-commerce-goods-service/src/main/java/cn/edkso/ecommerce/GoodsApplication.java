84827222769b053202459ba38f61cfc514548d13
lincheng <1498047434@qq.com>
2023-05-14 23:58:39 +0800

    【e-commerce-goods-service】GoodsApplication.java 增加描述

--- e-commerce-service/e-commerce-goods-service/src/main/java/cn/edkso/ecommerce/GoodsApplication.java
+++ e-commerce-service/e-commerce-goods-service/src/main/java/cn/edkso/ecommerce/GoodsApplication.java
@@ -9,7 +9,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
  * @author dingmengdi
  * @version 1.0
  * @date 2023-04-23 11:54
- * @description
+ * @description 商品微服务启动入口
+ *
+ *  * 启动依赖组件/中间件: Redis + MySQL + Nacos + Kafka + Zipkin
+ *  * http://127.0.0.1:8001/ecommerce-goods-service/doc.html
  */
 
 @EnableJpaAuditing
